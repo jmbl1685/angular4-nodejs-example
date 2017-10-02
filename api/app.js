9d3a94ddb5ae4068cde3213bf23db0e6c5396bfe
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2017-10-02 07:58:49 -0500

    Cambio app.js

--- api/app.js
+++ api/app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const express = require ('express'),  
-      bodyParser = require ('body-parser'),
-      routes = require ('./routes/routes'),
-      app = express()
+const express = require ('express')
+const bodyParser = require ('body-parser')
+const routes = require ('./routes/routes')
+const app = express()
 
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
